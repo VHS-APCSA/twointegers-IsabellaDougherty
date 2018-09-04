4dc037020f4ef96667958e4a27dfd1a989a86f3c
bob <bob@APCSA>
2018-09-04 15:38:40 -0700

    Finally made the linked TwoIntegers class

---
+++ src/TwoIntegers.java
@@ -0,0 +1,34 @@
+public class TwoIntegers 
+{
+	private int numone;
+	private int numtwo;
+	
+	public TwoIntegers(int numone, int numtwo)
+	{
+		numone = 0;
+		numtwo = 0;
+	}
+	
+	public void TwoIntegers (int numone, int numtwo)
+	{
+		this.numone = numone;
+		this.numtwo = numtwo;
+	}
+	
+	public int getNumone()
+	{
+		return numone;
+	}
+	public void setNumone(int numone)
+	{
+		this.numone = numone;
+	}
+	public int getNumtwo()
+	{
+		return numtwo;
+	}
+	public void setNumtwo(int numtwo)
+	{
+		this.numtwo = numtwo;
+	}
+}
